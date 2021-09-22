1a6b2f4b62df1da89ebb80c93af539a673350c51
TostaMistaEXE <60255268+TostaMistaEXE@users.noreply.github.com>
2021-09-22 11:11:25 +0100

    CsLogin

---
+++ LoginRememberMe/LoginRememberMe/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace LoginRememberMe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_user = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.button_login = new System.Windows.Forms.Button();
+            this.cb_remember = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // tb_user
+            // 
+            this.tb_user.Location = new System.Drawing.Point(82, 57);
+            this.tb_user.Name = "tb_user";
+            this.tb_user.Size = new System.Drawing.Size(100, 22);
+            this.tb_user.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // tb_password
+            // 
+            this.tb_password.Location = new System.Drawing.Point(82, 107);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.Size = new System.Drawing.Size(100, 22);
+            this.tb_password.TabIndex = 3;
+            // 
+            // button_login
+            // 
+            this.button_login.Location = new System.Drawing.Point(82, 163);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(75, 23);
+            this.button_login.TabIndex = 4;
+            this.button_login.Text = "Login";
+            this.button_login.UseVisualStyleBackColor = true;
+            this.button_login.Click += new System.EventHandler(this.button_login_Click);
+            // 
+            // cb_remember
+            // 
+            this.cb_remember.AutoSize = true;
+            this.cb_remember.Location = new System.Drawing.Point(85, 136);
+            this.cb_remember.Name = "cb_remember";
+            this.cb_remember.Size = new System.Drawing.Size(122, 21);
+            this.cb_remember.TabIndex = 5;
+            this.cb_remember.Text = "Remember me";
+            this.cb_remember.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cb_remember);
+            this.Controls.Add(this.button_login);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_user);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_user;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.Button button_login;
+        private System.Windows.Forms.CheckBox cb_remember;
+    }
+}
+
---
+++ LoginRememberMe/LoginRememberMe/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginRememberMe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tb_user.Text = Properties.Settings.Default.User;
+            tb_password.Text = Properties.Settings.Default.Password;
+            if(Properties.Settings.Default.Remember == true)
+                cb_remember.Checked = true;
+            else
+                cb_remember.Checked = false;
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            if(String.IsNullOrEmpty(tb_user.Text) == true || String.IsNullOrEmpty(tb_password.Text) == true)
+            {
+                MessageBox.Show("fill all fields");
+                return;
+            }
+                if (cb_remember.Checked == true)
+                { 
+                    Properties.Settings.Default.User = tb_user.Text;
+                    Properties.Settings.Default.Password = tb_password.Text;
+                    Properties.Settings.Default.Remember = true;
+                    Properties.Settings.Default.Save();
+                }
+                else
+                { 
+                    Properties.Settings.Default.Reset();
+                    Properties.Settings.Default.Save();
+                }
+            MessageBox.Show("Successfully logged in");
+            Application.Exit();
+        }
+    }
+}
